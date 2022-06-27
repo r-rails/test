d20509d8e9d04b9d6ec2eb61355e86d22763687e
grace <chanheeya@gmail.com>
2022-06-27 18:50:30 -0500

    made "/grace" page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def grace
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
+  
+  get "/grace", to: "home#grace"
 end
