b7dd6a435c865834c84cd2b4f884da5eedce74f2
Daniel Täsch <accounts@datae.de>
2022-06-28 20:07:25 +0200

    add members page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
-  
-  get "/grace", to: "home#grace"
-  get "/uzorjchibuzor", to: "home#uzorjchibuzor"
   get "/links", to: "home#links"
+  
+  scope "members", as: 'members' do
+    get "/", to: "home#members"
+    get "/grace", to: "home#grace"
+    get "/uzorjchibuzor", to: "home#uzorjchibuzor"
+  end
 end
